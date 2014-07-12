63796db084b9cdc680625fefff68319a838c44e8
Bob Forma <bobforma@gmail.com>
2014-07-12 16:18:35 +0200

    More compact opponent listing

--- app/assets/javascripts/views/games/opponent.js.jsx
+++ app/assets/javascripts/views/games/opponent.js.jsx
@@ -6,15 +6,7 @@ RoboRacer.Views.Opponent = React.createBackboneClass({
 
     return (
       <li className={ className }>
-        <h3>{ this.getModel().get('name') }</h3>
-
-        <ul className="slots">
-          <li className="slot"><span className="card_empty" /></li>
-          <li className="slot"><span className="card_empty" /></li>
-          <li className="slot"><span className="card_empty" /></li>
-          <li className="slot"><span className="card_empty" /></li>
-          <li className="slot"><span className="card_empty" /></li>
-        </ul>
+        { this.getModel().get('name') }
       </li>
     );
   }
