e528508397c62e61d5f8670701d396734c5e63eb
Bob Forma <bobforma@gmail.com>
2015-03-22 11:01:18 +0100

    Initial setup for new design
    - color scheme (goldfish)
    - page header (that's nicely aligned)
    - page footer (that sticks to the bottom)
    - new landing page

--- spec/features/player_feature_spec.rb
+++ spec/features/player_feature_spec.rb
@@ -4,12 +4,12 @@ describe Players::RegistrationsController do
   it "registers a new player account" do
     visit root_path
 
-    click_on I18n.t("home.index.new_player")
+    click_on "Register"
     fill_in "player[name]", with: "Bob"
     fill_in "player[email]", with: "bob@localhost.local"
     fill_in "player[password]", with: "secret"
     fill_in "player[password_confirmation]", with: "secret"
-    click_on I18n.t("devise.registrations.new.sign_up")
+    click_on "Sign up"
 
     expect(page).to have_content "Your account has been created successfully."
   end
